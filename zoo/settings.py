b00d8af5a6d086369cf939e66884bd377fdf8333
ismyblue <ismyblue@163.com>
2020-11-04 11:41:46 +0800

    delete django zoowebend, create another vue zoowebend project

--- zoo/settings.py
+++ zoo/settings.py
@@ -32,8 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # 'zooapi.apps.ZooapiConfig',
-    'zooapi',
-    'zoowebend',
+    'zooapi',    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
